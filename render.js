c3852ae781ead5312de154b2797f94d781a58f33
Shanfan Huang <Shanfan@users.noreply.github.com>
2023-11-05 18:42:27 -0500

    Create reference links utility

--- render.js
+++ render.js
@@ -1,61 +1,59 @@
-import { PersonLifetime } from './utils/mapLifetime.js'
-import { layYearMarks, renderWar } from './utils/mapMetaMarks.js'
-import utzonData from './data/utzon.json' assert {type: "json"}
-import gehryData from './data/gehry.json' assert {type: "json"}
-import getDimensions from './utils/getDimensions.js'
+// import { PersonLifetime } from './utils/mapLifetime.js'
+// import { layYearMarks, renderWar } from './utils/mapMetaMarks.js'
+// import utzonData from './data/utzon.json' assert {type: "json"}
+// import gehryData from './data/gehry.json' assert {type: "json"}
+// import getDimensions from './utils/getDimensions.js'
+
+import createRefLinks from "./utils/createRefLinks.js";
 
 
-export const scale = d3.scaleLinear()
-    .range([0, getDimensions('.visCanvas').height])
-    .domain([1912, 2022])
+// export const scale = d3.scaleLinear()
+//     .range([0, getDimensions('.visCanvas').height])
+//     .domain([1912, 2022])
 
-export const utzonEvents = getEventsData(utzonData)
-export const gehryEvents = getEventsData(gehryData)
+// export const utzonEvents = getEventsData(utzonData)
+// export const gehryEvents = getEventsData(gehryData)
 
-function getEventsData(data) {
-    const events = []
-    events.push(data.birth, data.death, data.pritzker)
+// function getEventsData(data) {
+//     const events = []
+//     events.push(data.birth, data.death, data.pritzker)
 
-    data.primeProject.events.forEach(d => {
-        events.push({ "year": d.year, "description": d.description })
-    })
+//     data.primeProject.events.forEach(d => {
+//         events.push({ "year": d.year, "description": d.description })
+//     })
 
-    if (data.secondaryProject) {
-        data.secondaryProject.events.forEach(d => {
-            events.push({ "year": d.year, "description": d.description })
-        })
-    }
+//     if (data.secondaryProject) {
+//         data.secondaryProject.events.forEach(d => {
+//             events.push({ "year": d.year, "description": d.description })
+//         })
+//     }
 
-    data.otherEvents.forEach(d => {
-        events.push({ "year": d.year, "description": d.description })
-    })
+//     data.otherEvents.forEach(d => {
+//         events.push({ "year": d.year, "description": d.description })
+//     })
 
-    return events
-}
+//     return events
+// }
 
-document.querySelector('.utzon.content p').innerHTML = utzonEvents.find(event => event.year === 1918).description
-document.querySelector('.gehry.content p').innerHTML = gehryEvents.find(event => event.year === 1929).description
+// document.querySelector('.utzon.content p').innerHTML = utzonEvents.find(event => event.year === 1918).description
+// document.querySelector('.gehry.content p').innerHTML = gehryEvents.find(event => event.year === 1929).description
 
-layYearMarks('.visCanvas .left .yearMarks', scale);
-layYearMarks('.visCanvas .right .yearMarks', scale);
+// layYearMarks('.visCanvas .left .yearMarks', scale);
+// layYearMarks('.visCanvas .right .yearMarks', scale);
 
-renderWar('.visCanvas .left .wwi', 1914, 1918, scale)
-renderWar('.visCanvas .right .wwi', 1914, 1918, scale)
-renderWar('.visCanvas .left .wwii', 1937, 1945, scale)
-renderWar('.visCanvas .right .wwii', 1937, 1945, scale)
+// renderWar('.visCanvas .left .wwi', 1914, 1918, scale)
+// renderWar('.visCanvas .right .wwi', 1914, 1918, scale)
+// renderWar('.visCanvas .left .wwii', 1937, 1945, scale)
+// renderWar('.visCanvas .right .wwii', 1937, 1945, scale)
 
-// renderCareerMark('.early.career', 1952)
-// renderCareerMark('.mid.career', 1967)
-// renderCareerMark('.late.career', 1985)
-// renderCareerMark('.legacy.career', 2000)
+// const utzon = new PersonLifetime(utzonData, '.visCanvas .utzon', scale)
+// const gehry = new PersonLifetime(gehryData, '.visCanvas .gehry', scale)
 
-const utzon = new PersonLifetime(utzonData, '.visCanvas .utzon', scale)
-const gehry = new PersonLifetime(gehryData, '.visCanvas .gehry', scale)
-
-utzon.drawEvents('prime')
-utzon.drawEvents('other')
-gehry.drawEvents('prime')
-gehry.drawEvents('secondary')
-gehry.drawEvents('other')
+// utzon.drawEvents('prime')
+// utzon.drawEvents('other')
+// gehry.drawEvents('prime')
+// gehry.drawEvents('secondary')
+// gehry.drawEvents('other')
 
 
+createRefLinks('.references')
---
+++ utils/createRefLinks.js
@@ -0,0 +1,85 @@
+export default function createRefLinks(selector) {
+
+    function createLinks(content, href) {
+        const link = document.createElement('a')
+        link.href = href
+        link.target = "_blank"
+        link.innerHTML = content
+
+        return link
+    }
+
+    const references = [
+        {
+            'title': 'I&rsquo;m Frank Gehry, and This Is How I See the World - Los Angeles Times (1992)',
+            'href': 'https://www.latimes.com/archives/la-xpm-1998-oct-25-tm-35829-story.html'
+        },
+        {
+            'title': 'Frank Gehry, 1990 TED Talk, My Days as A Young Rebel',
+            'href': 'https://www.ted.com/talks/frank_gehry_my_days_as_a_young_rebel'
+        },
+        {
+            'title': 'The Software Behind Frank Gehry’s Geometrically Complex Architecture - Priceonomics',
+            'href': 'https://priceonomics.com/the-software-behind-frank-gehrys-geometrically/'
+        },
+        {
+            'title': 'Gehry Residence / Gehry Partners | ArchDaily',
+            'href': 'https://www.archdaily.com/67321/gehry-residence-frank-gehry'
+        },
+        {
+            'title': 'Walt Disney Concert Hall - Wikipedia',
+            'href': 'https://en.wikipedia.org/wiki/Walt_Disney_Concert_Hall'
+        },
+        {
+            'title': 'List of Works by Frank Gehry - Wikipedia',
+            'href': 'https://en.wikipedia.org/wiki/List_of_works_by_Frank_Gehry'
+        },
+        {
+            'title': 'Sydney Opera House - Wikipedia',
+            'href': 'https://en.wikipedia.org/wiki/Sydney_Opera_House'
+        },
+        {
+            'title': 'The Bilbao Effect : How the Design of a Museum Transformed The Economy of The City',
+            'href': 'https://www.snaptrude.com/blog/the-bilbao-effect-how-the-design-of-a-museum-transformed-the-economy-of-the-city'
+        },
+        {
+            'title': 'Sydney Opera House | Our Story',
+            'href': 'https://www.sydneyoperahouse.com/our-story/'
+        },
+        {
+            'title': 'Utzon’s Hellebæk Residence in Denmark by Jørn Utzon',
+            'href': 'https://archeyes.com/utzons-house-in-hellebaek-denmark-jorn-utzon/'
+        },
+        {
+            'title': 'Look inside Jørn Utzon&rsquo;s Can Lis Family House from the 70s in Mallorca',
+            'href': 'https://www.designboom.com/architecture/jorn-utzon-can-lis-family-home-mallorca-08-02-2019/'
+        },
+        {
+            'title': 'Can Feliz, Mallorca - Jørn Utzon | Arquitectura Viva',
+            'href': 'https://arquitecturaviva.com/works/can-feliz-9'
+        },
+        {
+            'title': 'AD Classics: Bagsværd Church / Jørn Utzon | ArchDaily',
+            'href': 'https://www.archdaily.com/160390/ad-classics-bagsvaerd-church-jorn-utzon'
+        },
+        {
+            'title': 'The Utzon Center: Jørn Utzon&rsquo;s Architectural Legacy in Aalborg',
+            'href': 'https://archeyes.com/utzon-center-aalborg-jorn-utzon/'
+        },
+        {
+            'title': "15 Iconic Projects by Jørn Utzon",
+            'href': 'https://www.re-thinkingthefuture.com/know-your-architects/a360-15-iconic-projects-by-jorn-utzon-architect-of-sydney-opera-house/'
+        },
+        {
+            'title': 'Architecture Classics: Kuwait National Assembly Building / Jørn Utzon | ArchDaily',
+            'href': 'https://www.archdaily.com/568821/ad-classics-kuwait-national-assembly-building-jorn-utzon'
+        }
+    ]
+
+    references.forEach(d => {
+        const link = createLinks(d.title, d.href)
+        const list = document.createElement('li')
+        list.append(link)
+        document.querySelector(selector).appendChild(list)
+    })
+}
