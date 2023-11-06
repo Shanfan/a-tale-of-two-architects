4ae61c38d682db9f7fd08b7c84b9ad37dc8eea6b
Shanfan Huang <Shanfan@users.noreply.github.com>
2023-11-06 10:48:14 -0500

    positioning narration content

--- render.js
+++ render.js
@@ -1,23 +1,26 @@
 import getDimensions from './utils/getDimensions.js'
 import createRefLinks from "./utils/createRefLinks.js";
-import { utzonNarratives } from "../data/utzon.js";
-import { gehryNarratives } from "../data/gehry.js";
+import { utzonChapter1, utzonChapter2 } from "../data/utzon.js";
+import { gehryChapter1, gehryChapter2 } from "../data/gehry.js";
 import mapNarratives from "./utils/mapNarratives.js";
 
 
+// render function should run on window change, 
+// so that all the positioning gets recalculated 
 
-
-// then, adjust their y position 
-// this will differ from chapter to chapter. 
-// So I think we should create a separate function 
 
 function mapChapter1() {
 
-    mapNarratives('.chapter1 .utzon', utzonNarratives)
-    mapNarratives('.chapter1 .gehry', gehryNarratives, false)
+    // todo: mapNarratives can get written to include both utzon & gehry
+    mapNarratives('.chapter1 .utzon', utzonChapter1)
+    mapNarratives('.chapter1 .gehry', gehryChapter1)
+
+
     const narrations = document.querySelectorAll('.chapter1 .narrationWrapper')
+
     const paddingTop = getDimensions(narrations[0]).height / 2
     const narrationGap = 15
+
     const totalWidth = getDimensions('.chapter1 .visCanvas').width
     let totalHeight = paddingTop + narrationGap * narrations.length, years = []
 
@@ -30,16 +33,15 @@ function mapChapter1() {
         .range([0, totalHeight])
         .domain(d3.extent(years))
 
+    // Draw SVG life line & circle
     const visCanvas = d3.select('.chapter1 .visCanvas').append('svg')
         .attr('width', '100%')
         .attr('height', totalHeight)
         .append('g')
         .style('transform', `translateY(${paddingTop}px)`)
 
-
     const utzonLife = visCanvas.append('g').attr('class', 'utzon')
         .style('transform', `translateX(${narrationGap}px)`)
-
 
     utzonLife.append('path')
         .attr('d', `M0,${scale(1918)} V${totalHeight - paddingTop}`)
@@ -55,8 +57,6 @@ function mapChapter1() {
     const gehryLife = visCanvas.append('g').attr('class', 'gehry')
         .style('transform', `translateX(${totalWidth - narrationGap}px)`)
 
-    console.log(totalWidth)
-
     gehryLife.append('path')
         .attr('d', `M0,${scale(1929)} V${totalHeight - paddingTop}`)
         .attr('stroke-width', '1')
@@ -67,6 +67,24 @@ function mapChapter1() {
         .attr('fill', 'none')
         .attr('stroke-width', 2)
         .attr('cy', scale(1929))
+
+    // translateY for each narrationWrapper
+    const gehryNarrations = document.querySelectorAll('.chapter1 .gehry .narrationWrapper')
+    gehryNarrations.forEach(el => {
+        const year = +el.getAttribute('data-year')
+        const dist = scale(year) - getDimensions(el).height / 2 + paddingTop
+        el.style.setProperty('transform', `translateY(${dist}px)`)
+    })
+
+
+    const utzonNarrations = document.querySelectorAll('.chapter1 .utzon .narrationWrapper')
+    utzonNarrations.forEach(el => {
+        el.classList.add('narrationWrapperLeft');
+        const year = +el.getAttribute('data-year')
+        const dist = scale(year) - getDimensions(el).height / 2 + paddingTop
+
+        el.style.setProperty('transform', `translateY(${dist}px)`)
+    })
 
 }
 
--- utils/mapNarratives.js
+++ utils/mapNarratives.js
@@ -1,31 +1,28 @@
-const getSize = (n, isLeft) => {
-    if (n < 100) {
-        return isLeft ? 'grid__colspan_4 grid__colpush_2' : 'grid__colspan_4'
-    } else if (n > 550) {
-        return 'grid__colspan_6'
-    } else {
-        return isLeft ? 'grid__colspan_5 grid__colpush_1' : 'grid__colspan_5'
+export default function mapNarratives(selector, narratives) {
+    const getSize = (n) => {
+        if (n < 100) {
+            return 'narrationWrapperS'
+        } else if (n > 550) {
+            return 'narrationWrapperL'
+        } else {
+            return 'narrationWrapperM'
+        }
     }
-}
-
-export default function mapNarratives(selector, narratives, isLeft = true) {
 
     narratives.forEach(d => {
-
         const dataYear = document.createAttribute('data-year')
         dataYear.value = d.year
 
-        const sizeWrapper = document.createElement('div')
-        sizeWrapper.classList = getSize(d.content.length, isLeft)
-
         const narrationWrapper = document.createElement('div')
         narrationWrapper.className = 'narrationWrapper'
+        narrationWrapper.classList.add(getSize(d.content.length))
+
+
         narrationWrapper.setAttributeNode(dataYear)
 
         narrationWrapper.innerHTML = d.content
-        sizeWrapper.append(narrationWrapper)
 
-        document.querySelector(selector).append(sizeWrapper);
+        document.querySelector(selector).append(narrationWrapper);
     })
 
 }
